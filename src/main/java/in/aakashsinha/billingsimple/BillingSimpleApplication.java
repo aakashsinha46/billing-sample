f656b1cad6c3b905803a22d6a4293e9753f2ae62
Aakash Sinha <sinha@Aakashs-MacBook-Air.local>
2023-11-17 11:39:04 +0530

    init

---
+++ src/main/java/in/aakashsinha/billingsimple/BillingSimpleApplication.java
@@ -0,0 +1,13 @@
+package in.aakashsinha.billingsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/aakashsinha/billingsimple/BillingSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aakashsinha.billingsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
